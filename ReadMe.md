b30e86c57ec84500dc01350ed8b450e738101ee4
Mel Wey <45990429+melwey@users.noreply.github.com>
2024-04-17 16:18:01 +0200

    improve description

--- ReadMe.md
+++ ReadMe.md
@@ -3,16 +3,17 @@ Selection of EU-HYDI data for publication and export to csv.
 
 EU-HYDI stands for the **EUropean HYdrological Data Inventory**. It is a dataset of soil hydrological, physical and chemical properties assembled in 2013 in the context of EU FP7 project My Water. It contains data from more than 18000 soil samples contributed by 27 institutions in Europe. The compilation work was led by Mélanie Weynants at the Joint Research Centre of the European Commission (JRC).
 
+We are working towards making the database available under CC-BY license, without success so far. 
 Some but not all contributors have agreed to make their data publicly accessible through the [European Soil Data Centre (ESDAC)](https://esdac.jrc.ec.europa.eu) of the JRC. This repository contains the R code to filter the original dataset and extract the data that may be distributed publicly on ESDAC as a set of csv files.
 
 Table: Status of EU-HYDI contributions
 
-|Contributor                                                                                                                    |Access     |Source in EU-HYDI   | N profiles| N samples|
+|Contributor                                                                                                                    |Access     |Source in HYDI   | N profiles| N samples|
 |:------------------------------------------------------------------------------------------------------------------------------|:----------|:----------------|----------:|---------:|
 |Belgium: Earth and Life Institute, UCLouvain                                                                                   |public     |Javaux           |         11|        44|
 |Belgium: Ghent University                                                                                                      |public     |Cornelis         |        120|       241|
-|Germany: ZALF Müncheberg                                                                                                       |public     |Schindler_HYPRES |         52|       471|
 |Germany: ZALF Müncheberg                                                                                                       |public     |Schindler        |         32|       298|
+|Germany: ZALF Müncheberg                                                                                                       |public     |Schindler_HYPRES |         52|       471|
 |Hungary: University of Pannonia                                                                                                |public     |Mako             |        308|       900|
 |Italy: University of Palermo                                                                                                   |public     |Iovino           |        375|       417|
 |Netherlands: Wageningen University                                                                                             |public     |Wosten_HYPRES    |        102|       358|
@@ -28,16 +29,21 @@ Table: Status of EU-HYDI contributions
 |Germany: BGR                                                                                                                   |restricted |Hennings_HYPRES  |        518|      1527|
 |Greece: Aristotle University of Thessaloniki                                                                                   |restricted |Bilas            |        883|      2588|
 |Italy: University of Naples Federico II                                                                                        |restricted |Romano           |        451|       623|
+|Italy: University of Naples Federico II                                                                                        |restricted |Romano_HYPRES    |        155|       216|
 |Italy: University of Padova                                                                                                    |restricted |Morari           |          5|        14|
-|Norway: Bioforsk Soil and Environment                                                                                          |restricted |Kvaerno          |        504|      2033|
-|Norway: Norwegian Forest and Landscape Institute                                                                               |restricted |Kvaerno          |        504|      2033|
-|Norway: Norwegian University of Life Sciences                                                                                  |restricted |Kvaerno          |        504|      2033|
-|Norway: Nowegian Water Resources and Energy Directorate                                                                        |restricted |Kvaerno          |        504|      2033|
-|Slovakia: Soil Fertility Research Institute                                                                                    |restricted |Houskova         |         22|        97|
-|Slovakia: Soil Science and Conservation Research Institute                                                                     |restricted |Houskova_HYPRES  |         14|        58|
+|Norway: Bioforsk Soil and Environment                                                                                          |restricted |Kvaerno          |        283|      1091|
+|Norway: Norwegian Forest and Landscape Institute                                                                               |restricted |Kvaerno          |        109|       325|
+|Norway: Norwegian University of Life Sciences                                                                                  |restricted |Kvaerno          |         87|       502|
+|Norway: Nowegian Water Resources and Energy Directorate                                                                        |restricted |Kvaerno          |         25|       115|
+|Slovakia: Soil Fertility Research Institute                                                                                    |restricted |Houskova_HYPRES  |         14|        58|
+|Slovakia: Soil Science and Conservation Research Institute                                                                     |restricted |Houskova         |         22|        97|
 |Spain: Evenor-Tech                                                                                                             |restricted |Anaya            |       1081|      3787|
 |United Kingdom: Cranfield University                                                                                           |restricted |Cranfield        |        119|       426|
 
-# Reference
+## Additional contributions
+
+New data can be contributed to the database under CC-BY license. Please open an issue in this github repository or contact [ec-esdac@ec.europa.eu](mailto:ec-esdac@ec.europa.eu,panos.panagos@ec.europa.eu?subject=Dataset%20Help%20Desk%20-%20European%20Hydropedological%20Data%20Inventory%20(EU-HYDI)%20database).
+
+## Reference
 Weynants, M., Montanarella, L., Tóth, G., Arnoldussen, A., Anaya Romero, M., Bilas, G., Borresen T., Cornelis W., Daroussin J., Gonalves M., Hannam J., Haugen L.E., Hennings V., Houskova B., Iovino M., Javaux M., Keay C.A., Kätterer T., Kvaerno S., Laktinova T., Lamorski K., Lilly A., Mako A., Matula S., Morari F., Nemes A., Patyka N.V., Romano N., Schindler U., Shein E., Slawinski C., Strauss P, Tóth B., Wösten, H. (2013). European HYdropedological data inventory (EU-HYDI). EUR – Scientific and Technical Research series – vol. EUR 26053 EN. Publications Office of the European Union, Luxembourg, GD Luxembourg. https://doi.org/10.2788/5936
     
--- status.R
+++ status.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 
 # load EUHYDI_v1_1.Rdata
-p2h <- readline("Path to EUHYDI_v1_1.Rdata")
-load(p2h)
-for (name in names(hydi)){
-  expres <- parse(text = paste0(tolower(name), " <- hydi$", name), keep.source = FALSE)
-  print(expres)
+version <- "v1.1.0"
+p2h <- readline("Path to EUHYDI_v1_1_csv: ")
+files <- dir(p2h)
+for (f in files) {
+  name <- sub(".csv", "", f) %>% tolower()
+  expres <- parse(text = paste0(name, "<- read_csv(file.path(p2h, f))"), keep.source = FALSE, encoding = "UTF-8")
   eval(expres)
 }
 
@@ -60,11 +61,12 @@ status$source <- c(
   "Kvaerno",
   "Kvaerno",
   "Goncalves",
-  "Houskova",
   "Houskova_HYPRES",
+  "Houskova",
   "Anaya",
   "Cranfield"
 )
+
 status$source %in% sources
 
 # which sources need to be extracted?
@@ -93,6 +95,14 @@ Ksat %>%
   nrow()
 
 
+## prepare for export
+hydi <- vector("list", length(files))
+names(hydi) <- gsub(".csv","",files)
+for (f in files){
+  name = sub(".csv","",f)
+  eval(parse(text = paste0("hydi[[name]] <- ", tolower(name))))
+}
+
 ## filter sources
 filter_fun <- function(df){
   if ("SOURCE" %in% names(df)) {
@@ -105,57 +115,150 @@ filter_fun <- function(df){
 
 exp_hydi <- lapply(hydi, filter_fun)
 
-set_na <- function(x){x[] <- -999}
+set_na <- function(x){
+  if (is.character(x)) {
+    x[] = "ND"
+  } else {
+    x[] <- -999
+  }
+}
 
 # remove coord
 gen_nogeog <- exp_hydi$GENERAL %>%
-    filter(SOURCE %in% exp_nogeog$source) %>%
-    mutate(across(LOC_COOR_X:Y_WGS84, set_na))
+  filter(SOURCE %in% exp_nogeog$source) %>%
+  mutate(across(LOC_COOR_X:Y_WGS84, set_na)) %>%
+  mutate(COMMENTS_PBL = "Coordinates removed from public version.")
 
 exp_hydi$GENERAL <- 
   bind_rows(
-    exp_hydi$GENERAL %>% filter(! SOURCE %in% exp_nogeog$source),
+    exp_hydi$GENERAL %>% 
+      mutate(COMMENTS_PBL = "ND") %>% 
+      filter(! SOURCE %in% exp_nogeog$source),
     gen_nogeog
   ) %>%
   arrange(SOURCE, PROFILE_ID) 
 
 
 # export to csv
-if (!dir.exists("./euhydi_public_csv")) {dir.create("./euhydi_public_csv")}
+if (!dir.exists(paste0("./EUHYDI_public_",version,"_csv"))) {dir.create(paste0("./EUHYDI_public_", version, "_csv"))}
 for (name in names(hydi)){
-  write_csv(exp_hydi[[name]], paste0("./euhydi_public_csv/", name, ".csv"))
+  # use write_excel_csv to include a UTF-8 Byte order mark which indicates to Excel the csv is UTF-8 encoded
+  write_excel_csv(
+    exp_hydi[[name]],
+    file = paste0("./EUHYDI_public_", version, "_csv/", name, ".csv"),
+    )
 }
 
 # copy references and metadata from "../EUHYDI_v1_1_csv"
-file.copy(from = "../eu_hydi_build/output/EUHYDI_v1_1_csv/METADATA.csv", to = "./euhydi_public_csv/METADATA.csv")
-file.copy(from = "../eu_hydi_build/output/EUHYDI_v1_1_csv/REFERENCES.csv", to = "./euhydi_public_csv/REFERENCES.csv")
+file.copy(from = paste0(p2h,"/METADATA.csv"), to = paste0("./EUHYDI_public_", version, "_csv/METADATA.csv"))
+file.copy(from = paste0(p2h,"/REFERENCES.csv"), to = paste0("./EUHYDI_public_", version, "_csv/REFERENCES.csv"))
 
 
 # Norway
-general %>% filter(SOURCE == "Kvaerno") %>% group_by(CONTACT_P) %>% count()
+institutions <- tibble(Contributor = c(
+    "Norway: Bioforsk Soil and Environment",
+    "Norway: Bioforsk Soil and Environment",
+    "Norway: Norwegian Forest and Landscape Institute",
+    "Norway: Norwegian University of Life Sciences",
+    "Norway: Nowegian Water Resources and Energy Directorate",
+    "Spain: Evenor-Tech",
+    "Greece: Aristotle University of Thessaloniki",
+    "Belgium: Ghent University",
+    "United Kingdom: Cranfield University",
+    "France: INRA, Orléans",
+    "Portugal: Instituto Nacional de Investigação Agrária e Veterinária",
+    "Germany: BGR",
+    "Slovakia: Soil Science and Conservation Research Institute",
+    "Slovakia: Soil Fertility Research Institute",
+    "Italy: University of Palermo",
+    "Belgium: Earth and Life Institute, UCLouvain",
+    "Sweden: Swedish University of Agricultural Sciences",
+    "Poland: Institute of Agrophysics, Polish Academy of Sciences, Lublin",
+    "United Kingdom: James Hutton Institute",
+    "Hungary: University of Pannonia",
+    "Czechia: Czech University of Life Science in Prague",
+    "Italy: University of Padova",
+    "Ukraine: National Scientific Center, Institute for Soil Science and Agrochemistry Research named after ON Sokolovskiy, Kharkiv",
+    "Italy: University of Naples Federico II",
+    "Italy: University of Naples Federico II",
+    "Germany: ZALF Müncheberg",
+    "Germany: ZALF Müncheberg",
+    "Germany: ZALF Müncheberg",
+    "Russian Federation: Moscow State University",
+    "Austria: Federal Agency for Water Management",
+    "Netherlands: Wageningen University"
+  ), CONTACT_A = c(
+    "Bioforsk East, Apelsvoll, Nylinna 226, N-2849 KAPP",
+    "Bioforsk Soil and Environment, Frederik A. Dahls vei 20, N-1432 Ås",
+    "Skog & Landskap, P.O. Box 115, N-1431 Ås",
+    "Norwegian University of Life Sciences, Department of Plant and Environmental Sciences, P.O. Box 5003, N-1432 Ås",
+    "Norwegian Water Resources and Energy Directorate, P.O. Box 5091 Majorstua, N-0301 Oslo",
+    "Evenor-Tech, CSIC Spin-off, Instituto de Recursos Naturales y Agrobiologia de Sevilla (CSIC), Avda. Reina Mercedes,10, 41012, Spain",
+    "Laboratory of Applied Soil Science, School of Agriculture, Aristotle University of Thessaloniki, 54124 Thessaloniki, Greece",                                   
+    "Ghent University",
+    "Bullock Building, Cranfield University, Cranfield, Bedfordshire, MK43 0PL",
+    "INRA, Orléans, France",
+    "Instituto Nacional de Investigação Agrária e Veterinária, Av. Republica, 2784-505 Oeiras, Portugal",
+    "Federal Institute for Geosciences and Natural Resources, BGR, Alfred-Bentz-Haus, Stilleweg 2, 30655 Hannover",
+    "VUPOP",
+    "Soil Fertility Research Institute, Gagarinova 10, 827 13 Bratislava, Slovakia. Fax:+42 7 295 487",
+    "Dipartimento dei Sistemi Agro-Ambientali, Università degli Studi di Palermo",
+    "Earth and Life Institute, Université catholique de Louvain",
+    "Swedish University of Agricultural Sciences",
+    "Institute of Agrophysics, Polish Academy of Sciences, Lublin",
+    "James Hutton Institute, Craigiebuckler, Aberdeen",
+    "Pannon Egyetem Georgikon Kar, Növénytermesztéstani és Talajtani Tanszék, H-8360 Keszthely Deák F. u. 16.",
+    "CULS Prague, Kamycka 129, Praha 6, 16521, CZ",
+    "Universitá di Padova",
+    "NSC ISSAR, Chaikovska str.,№4, Kharkiv, Ukraine 61024, tel: +380577041665; +380954011107",
+    "University of Naples Federico II",
+    "Institute of Agricultural Hydraulics, University of Naples \"Frederico II\", Universita 100, 80055 Portici, Napoli, Italy. tel: +39 81 488954",
+    "ZALF",
+    "Inst. of Hydrology, Centre for Research on Agric.Landscapes & Land Use (ZALF) e.V., 0-1278 Muncheberg, W.-Pieck-Str. 72 Germany. Tel: (033-432)82300",
+    "Zentrum fur Agarlandschafts-und Landnutzungsforschung(ZALF), Institut fur Bodenlandschaftsforschung, Eberswalder Str 84, 15374 Muncheberg. Fax 49 033432-82289",
+    "Moscow State University",
+    "Federal Agency for Water Management",
+    "Wageningen University & Research"
+    )
+)
+general %>% filter(SOURCE == "Kvaerno") %>% group_by(CONTACT_A) %>% count() %>% left_join(norway) %>% dplyr::select(!(CONTACT_A))
 
 # table status
 tbl <- status %>%
-  select(contributi, and_hereby, license_fi, comments, source) %>%
-  left_join(general %>% 
-              # group_by(SOURCE, ISO_COUNTRY) %>%
-              # count(substr(CONTACT_A, start=1, stop=30)) %>%
-              count(SOURCE) %>%
-              rename(`N profiles` = "n"),
-            by = c("source" = "SOURCE")
-            ) %>% 
-  left_join(basic %>% 
-              count(SOURCE) %>% 
+    select(contributi, and_hereby, license_fi, comments, source) %>%   full_join(general %>% 
+               group_by(SOURCE) %>%
+               # count(substr(CONTACT_A, start=1, stop=30)) %>%
+               count(CONTACT_A) %>%
+               # count(SOURCE) %>%
+               rename(`N profiles` = "n") %>%
+               full_join(institutions),
+             by = c("contributi" = "Contributor", "source" = "SOURCE")
+             ) %>%
+    left_join(
+            basic %>%
+              select(SAMPLE_ID, PROFILE_ID, SOURCE) %>%
+              # group_by(SOURCE) %>%
+              left_join(general %>% select(PROFILE_ID, CONTACT_A)) %>%
+              count(CONTACT_A) %>% 
               rename(`N samples` = "n"),
-            by = c("source" = "SOURCE")) %>%
-  mutate(Access = if_else(and_hereby == "accept" & !grepl("waiting", tolower(comments)), true = "public", false = "restricted", missing = "restricted"),
+            # by = c("source" = "SOURCE")
+            ) %>%
+  select(- CONTACT_A) %>%
+  mutate(Access = if_else(
+                    and_hereby == "accept" & !grepl("waiting", tolower(comments)), 
+                    true = "public", 
+                    false = "restricted", 
+                    missing = "restricted"
+                    ),
          .after = contributi) %>%
   select(- and_hereby) %>%
-  arrange(Access, contributi) %>%
   rename(Contributor = contributi,
          Licence_File = license_fi,
          Comment = comments,
          `Source in HYDI` = source) %>%
-  dplyr::select(-Licence_File, -Comment)
+  group_by(Contributor, Access, `Source in HYDI`) %>%
+  summarise(`N profiles` = sum(`N profiles`), `N samples` = sum(`N samples`)) %>%
+  arrange(Access, Contributor) 
+
 knitr::kable(tbl, caption = "Status of EU-HYDI contributions")
 knitr::kable(tbl, format = "html", caption = "Status of EU-HYDI contributions")
